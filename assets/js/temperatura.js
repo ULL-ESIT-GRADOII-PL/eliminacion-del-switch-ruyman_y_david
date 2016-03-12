373d0e71a5554b3fde9b3dcd8bf6f1e5a4132c1d
alu0100845235 <alu0100845235@ull.edu.es>
2016-03-12 23:30:35 +0000

    Eliminado el código Switch Smell

--- assets/js/temperatura.js
+++ assets/js/temperatura.js
@@ -1,57 +1,70 @@
 (function(exports) {
   "use strict";
 
+  // Clase Temperatura
   function Temperatura(valor,tipo)
   {
     Medida.call(this, valor, tipo);
-    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
   }
 
   Temperatura.prototype = new Medida();
   Temperatura.prototype.constructor = Temperatura;
 
+  // Clase Celsius
   function Celsius(valor)
   {
-    Temperatura.call(this,valor,"c");
-    /*celsius hereda de temperatura y llama al constructor ponindo por defecto la c en tipo*/
-    this.convFahrenheit = function() {
-      return ((valor * 9/5) + 32);
+    Temperatura.call(this, valor, "C");
+
+    this.toFahrenheit = function() {
+      return new Fahrenheit ((valor * 9/5) + 32);
     };
 
-    this.convKelvin = function() {
-      return (valor + 273.15);
+    this.toKelvin= function() {
+      return new Kelvin (valor + 273.15);
     };
   }
 
   Celsius.prototype = new Temperatura();
+  Celsius.prototype.MATCH = /(?:c(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?)/i;
   Celsius.prototype.constructor = Celsius;
+  Medida.measures["Celsius"] = Celsius;
 
+  // Clase Fahrenheit
   function Fahrenheit(valor)
   {
-    Temperatura.call(this,valor,"f");
-    this.convCelsius = function() {
-      return ((valor - 32) * 5/9);
+    Temperatura.call(this,valor,"F");
+
+    this.toCelsius = function() {
+      return new Celsius ((valor - 32) * 5/9);
     };
-    this.convKelvin = function() {
-      return((valor + 459.67) * 5/9);
+
+    this.toKelvin = function() {
+      return new Kelvin ((valor + 459.67) * 5/9);
     };
   }
 
   Fahrenheit.prototype = new Temperatura();
+  Fahrenheit.prototype.MATCH = /(?:f(?:a(?:h(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?)?)/i;
   Fahrenheit.prototype.constructor = Fahrenheit;
+  Medida.measures["Fahrenheit"] = Fahrenheit;
 
+  // Clase Kelvin
   function Kelvin(valor) {
-    Temperatura.call(this, valor, "k");
-    this.convCelsius = function() {
-      return(valor - 273.15);
+    Temperatura.call(this, valor, "K");
+
+    this.toCelsius = function() {
+      return new Celsius (valor - 273.15);
     };
-    this.convFahrenheit = function() {
-      return(valor * 9/5 - 459.67);
+
+    this.toFahrenheit = function() {
+      return new Fahrenheit ((valor * 9/5) - 459.67);
     };
   }
 
   Kelvin.prototype = new Temperatura();
+  Kelvin.prototype.MATCH = /(?:k(?:e(?:l(?:v(?:i(?:n)?)?)?)?)?)/i;
   Kelvin.prototype.constructor = Kelvin;
+  Medida.measures["Kelvin"] = Kelvin;
 
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
